f9656d872f51a8f20a600f8ed0c99990ce38c1ce
Tyler's Work <marquez.tym@icloud.com>
2022-08-08 07:32:23 -0700

    Final

---
+++ src/main/java/com/revature/SpringBootDemo/services/ShopService.java
@@ -0,0 +1,23 @@
+package com.revature.SpringBootDemo.services;
+
+
+import com.revature.SpringBootDemo.models.Shop;
+
+import java.util.List;
+
+public interface ShopService {
+
+    //    creates a new shop
+    boolean createShop(Shop shop);
+
+    //    get shop by id
+    Shop getShopById(int id);
+
+    //    get all shops
+    List<Shop> getAllShops();
+    //    update a shop
+    boolean updateShop(Shop shop);
+
+    //    delete a shop
+    boolean deleteShop(Shop shop);
+}
